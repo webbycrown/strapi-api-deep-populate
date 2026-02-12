33f1466eac7aadfce8f306bdf113e9df650131f9
rohit-ghoghari <rvghoghari@gmail.com>
2026-02-12 15:30:01 +0530

    logic change

--- dist/server/controllers/populate.js
+++ dist/server/controllers/populate.js
@@ -1,18 +1,19 @@
 "use strict";
 
 module.exports = ({ strapi }) => ({
- 
-
   async getSettings(ctx) {
     try {
-      const store = strapi.store({ type: "plugin", name: "strapi-plugin-api-deep-populate" });
+      const store = strapi.store({
+        type: "plugin",
+        name: "strapi-plugin-api-deep-populate",
+      });
       const savedSettings = (await store.get({ key: "settings" })) || {};
 
       const contentTypes = strapi.contentTypes;
       const response = {};
 
       Object.keys(contentTypes).forEach((uid) => {
-        if (uid.startsWith("api::")) {
+        if (uid.startsWith("api::") || uid.startsWith("plugin::")) {
           // Get the friendly name from the Content-Type info
           const displayName = contentTypes[uid].info.displayName;
 
@@ -37,7 +38,10 @@ module.exports = ({ strapi }) => ({
 
   async setSettings(ctx) {
     try {
-      const store = strapi.store({ type: "plugin", name: "strapi-plugin-api-deep-populate" });
+      const store = strapi.store({
+        type: "plugin",
+        name: "strapi-plugin-api-deep-populate",
+      });
 
       // OPTIONAL: Clean the data before saving so you don't save 'displayName' in the DB
       const dataToSave = { ...ctx.request.body };
